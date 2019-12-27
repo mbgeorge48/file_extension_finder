bb1a9181e0976a6dd684c6f05600a3f33a697a86
mbgeorge48 <mbgeorge48@gmail.com>
2019-12-27 20:15:18 +0000

    Adding a tiny bit of logic to stop it trying to copy files when there isn't enough room

--- find_files.py
+++ find_files.py
@@ -2,10 +2,13 @@ import os
 from shutil import copy
 import hashlib
 import logging
+import shutil
 from config_reader import ConfigReader
 
-TESTING = True
-FORMATTER = logging.Formatter("[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(lineno)s)", "%Y-%m-%d %H:%M:%S")
+TESTING = False
+FORMATTER = logging.Formatter(
+    "[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(lineno)s)", "%Y-%m-%d %H:%M:%S")
+
 
 class FindFiles:
     unique_file_paths = list()
@@ -19,11 +22,16 @@ class FindFiles:
         config = ConfigReader()
         self.extensions_to_find = config.extensions_to_find
         self.path_to_scan = os.path.normpath(config.path_to_scan)
-        self.path_to_copy_to =  os.path.normpath(config.path_to_copy_to)
-        if not  os.path.isdir("logs"):
+        self.path_to_copy_to = os.path.normpath(config.path_to_copy_to)
+
+        if not os.path.isdir("logs"):
             os.mkdir("logs")
-        self.file_info = self.set_logger( os.path.join("logs","file_info.log"), logging.DEBUG)
-        self.general =  self.set_logger( os.path.join("logs","general.log"), logging.DEBUG)
+        self.file_info = self.set_logger(os.path.join(
+            "logs", "file_info.log"), logging.DEBUG)
+        self.general = self.set_logger(os.path.join(
+            "logs", "general.log"), logging.DEBUG)
+
+        self.get_free_disk_space()
 
         self.scan_directories()
         self.move_files()
@@ -39,12 +47,18 @@ class FindFiles:
 
     def scan_directories(self):
         file_counter = 0
-        for root, dir, files in os.walk(self.path_to_scan): # We don't need to use dir
+        # We don't need to use dir
+        for root, dir, files in os.walk(self.path_to_scan):
             for file in files:
                 if os.path.splitext(file.lower())[-1] in self.extensions_to_find:
                     file_counter += 1
                     self.general.debug(f'Found file named: "{file}"')
-                    file_path =  os.path.join(root, file)
+                    file_path = os.path.join(root, file)
+                    self.free_space -= os.path.getsize(file_path)
+                    if self.free_space <= 0:
+                        self.general.error(
+                            f'Not enough room left on device - stopping before: "{file_path}"')
+                        break
                     hash_value = self.get_hash(file_path)
                     if hash_value not in self.hash_list or hash_value != 1:
                         self.hash_list.append(hash_value)
@@ -53,14 +67,14 @@ class FindFiles:
                     else:
                         self.dupe_file_paths.append(file_path)
                         self.general.info(f'"{file}" is DUPLICATE')
-                        self.general.debug(f'"{file}" has hash value of {hash_value}, which has already been allocated')
+                        self.general.debug(
+                            f'"{file}" has hash value of {hash_value}, which has already been allocated')
 
         self.general.info('#'*50)
         self.general.info(f'Found {file_counter} file in total')
         self.general.info(f'Found {len(self.unique_file_paths)} unique files')
         self.general.info(f'Found {len(self.dupe_file_paths)} duplicate files')
         self.general.info('#'*50)
-
 
     def get_hash(self, file_path):
         blocksize = 65536
@@ -74,16 +88,24 @@ class FindFiles:
             afile.close()
             return hasher.hexdigest()
         except PermissionError:
-            self.file_info.warning(f'Permission denied trying to read "{file_path}" marked it as a duplicate')
+            self.file_info.warning(
+                f'Permission denied trying to read "{file_path}" marked it as a duplicate')
             return 1
 
     def move_files(self):
         for file_path in self.unique_file_paths:
             if TESTING == True:
-                self.file_info.info(f'IN TESTING MODE: Moved "{file_path}" to "{self.path_to_copy_to}"')
+                self.file_info.info(
+                    f'IN TESTING MODE: Moved "{file_path}" to "{self.path_to_copy_to}"')
             else:
                 copy(file_path, os.path.normpath(self.path_to_copy_to))
-                self.file_info.info(f'Moved "{file_path}" to "{self.path_to_copy_to}"')
+                self.file_info.info(
+                    f'Moved "{file_path}" to "{self.path_to_copy_to}"')
+
+    def get_free_disk_space(self):
+        total, used, self.free_space = shutil.disk_usage(self.path_to_copy_to)
+        self.general.info("Free space on destination: %d GiB" %
+                          (self.free_space // (2**30)))
 
 
 if __name__ == "__main__":
